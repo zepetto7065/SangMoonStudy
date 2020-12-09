4771a4e2f2e57bc3c098e72cb05d14d137fe75bc
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-10 00:28:53 +0900

    node 공부 2일차

--- 알고리즘&자료구조/코딩테스트/프로그래머스/hash/완주하지 못한 선수.py
+++ 알고리즘&자료구조/코딩테스트/프로그래머스/hash/완주하지 못한 선수.py
@@ -11,3 +11,23 @@ def solution(participant, completion) :
 
 
 print(solution(participant, completion))
+
+
+#include <string>
+#include <vector>
+#include <algorithm>
+
+# using namespace std;
+#
+# string solution(vector<string> participant, vector<string> completion) {
+#     string answer = "";
+#     sort(participant.begin(), participant.end());
+#     sort(completion.begin(), completion.end());
+#     for(int i=0;i<completion.size();i++)
+#     {
+#         if(participant[i] != completion[i])
+#             return participant[i];
+#     }
+#     return participant[participant.size() - 1];
+#     //return answer;
+# }
