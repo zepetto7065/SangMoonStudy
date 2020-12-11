4ae6263c0b6476ad94ac03d1962b06b0acb8f27b
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-12 02:26:53 +0900

    React 강의 2일차
    - Body-Parser

--- ReactJS_study/boiler-plate/index.js
+++ ReactJS_study/boiler-plate/index.js
@@ -2,6 +2,14 @@ const express = require('express')
 const app = express()
 const port = 5000
 const mongoose = require('mongoose')
+const{User} = require('./models/User');
+const bodyParser = require('body-parser');
+
+//application/x-www-form-urlencoded
+app.use(bodyParser.urlencoded({extended:true})); 
+
+//application/json
+app.use(bodyParser.json);
 
 // mongoose 기본 설정
 mongoose.connect('mongodb+srv://nanum:welcome1@cluster0.2dhcx.mongodb.net/<dbname>?retryWrites=true&w=majority',{
@@ -11,4 +19,19 @@ mongoose.connect('mongodb+srv://nanum:welcome1@cluster0.2dhcx.mongodb.net/<dbnam
 
 app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
 
-app.listen(port, () => console.log('Example app listening on port ${port}!'))
+app.post('/register', (req, res) => {
+  //회원 가입할 떄 , 필요한 정보들을 클라이언트에서 가져오면 
+  //그것들을 DB에 넣어준다.
+  const user = new User(req.body)
+
+  user.save((err,userInfo)=>{
+    if(err) return res.json({ success:false, err})
+    return res.status(200).json({
+      success:true
+    })
+  })
+})
+
+
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
