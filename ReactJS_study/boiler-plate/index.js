5360057c1cb6fe811a17d04b470ae531bade0cfb
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-13 01:22:58 +0900

    3일차 강의
    - 비밀번호 암호화
    - 보안 정보 및 특정 보호해야할 정보 관리 파일 만들기

--- ReactJS_study/boiler-plate/index.js
+++ ReactJS_study/boiler-plate/index.js
@@ -1,34 +1,40 @@
 const express = require('express')
 const app = express()
 const port = 5000
-const mongoose = require('mongoose')
 const{User} = require('./models/User');
 const bodyParser = require('body-parser');
+const config = require('./config/key')
+
+
 
 //application/x-www-form-urlencoded
 app.use(bodyParser.urlencoded({extended:true})); 
 
 //application/json
-app.use(bodyParser.json);
+app.use(bodyParser.json());
 
 // mongoose 기본 설정
-mongoose.connect('mongodb+srv://nanum:welcome1@cluster0.2dhcx.mongodb.net/<dbname>?retryWrites=true&w=majority',{
-    useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true,useFindAndModify:false
+const mongoose = require('mongoose')
+
+mongoose.connect(config.mongoURI ,{
+  useNewUrlParser:true, useUnifiedTopology:true, useCreateIndex:true,useFindAndModify:false
 }).then(() => console.log('MongoDB Connected...'))
   .catch(err => console.log(err))
 
-app.get('/', (req, res) => res.send('Hello World! 안녕하세요'))
 
-app.post('/register', (req, res) => {
+
+app.get('/', (req, res) => res.send('Hello World! 안녕하세요!!!'))
+
+app.post('/register', (req, res) => { 
   //회원 가입할 떄 , 필요한 정보들을 클라이언트에서 가져오면 
   //그것들을 DB에 넣어준다.
   const user = new User(req.body)
 
   user.save((err,userInfo)=>{
-    if(err) return res.json({ success:false, err})
+    if(err) return res.json({ success:false, err});
     return res.status(200).json({
       success:true
-    })
+    });
   })
 })
 
--- ReactJS_study/boiler-plate/models/User.js
+++ ReactJS_study/boiler-plate/models/User.js
@@ -1,5 +1,7 @@
 const mongoose = require('mongoose');
-
+//bcrypt 사용법은 git 참조
+const bcrypt = require('bcrypt');
+const saltRound = 10; //salt가 몇글자인지
 
 const userShema = mongoose.Schema({
 
@@ -28,6 +30,25 @@ const userShema = mongoose.Schema({
         type : Number
     }
 })
+
+userShema.pre('save' , function(next){
+
+    var user = this; 
+
+    if(user.isModified('password')){
+        //비밀 번호를 암호화 시킨다.
+        bcrypt.genSalt(saltRound, function(err, salt){
+            if(err) return next(err);
+            bcrypt.hash(user.password, salt, function(err, hash){
+                if(err) return next(err)
+                user.password = hash           
+                next()
+            })
+        })    
+
+    }
+
+});
 
 const User = mongoose.model('User', userShema)
 module.exports = {User} //다른 곳에도 쓸 수 있도록
