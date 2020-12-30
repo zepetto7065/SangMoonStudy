44e0f4b796526a9149b58a7f0416cfa4687108da
zepetto.yoo <zepetto.yoo@gmail.com>
2020-12-30 14:15:59 +0900

    List Rest API 생성
    - Mysql 5.1.x 이후 버전에 따른 이슈 수정
    - 오탈자 수정

---
+++ ReactJS_study/react-spring/admin/src/main/java/com/user/admin/controller/EmployeeController.java
@@ -0,0 +1,25 @@
+package com.user.admin.controller;
+
+import com.user.admin.model.Employee;
+import com.user.admin.repository.EmployeeRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1")
+public class EmployeeController {
+
+    @Autowired
+    private EmployeeRepository employeeRepository;
+
+    @GetMapping("/employees")
+    private List<Employee> getAllEmployees(){
+        return employeeRepository.findAll();
+    }
+
+
+}
