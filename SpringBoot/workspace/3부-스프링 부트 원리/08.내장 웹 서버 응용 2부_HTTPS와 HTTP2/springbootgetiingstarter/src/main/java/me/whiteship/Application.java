190c2ab9737e98e47c7b916dfb595fb199971415
smyoo <smyoo@ubivelox.com>
2021-01-12 11:07:55 +0900

    스프링 부트 개념과 활용 정리
    - 내장 웹 서버 이해
    - 내장 웹 서버 응용 1부 : 컨테이너와 포트
    - 내장 웹 서버 응용 2부 : HTTP와 HTTP2

--- SpringBoot/workspace/3부-스프링 부트 원리/08.내장 웹 서버 응용 2부_HTTPS와 HTTP2/springbootgetiingstarter/src/main/java/me/whiteship/Application.java
+++ SpringBoot/workspace/3부-스프링 부트 원리/08.내장 웹 서버 응용 2부_HTTPS와 HTTP2/springbootgetiingstarter/src/main/java/me/whiteship/Application.java
@@ -2,14 +2,36 @@ package me.whiteship;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
 @RestController
 public class Application {
 
+    @GetMapping("/hello")
+    public String hello(){
+        return "hello Spring";
+    }
+
     public static void main(String[] args) {
         SpringApplication.run(Application.class, args);
     }
+
+
+    //connector add
+/*    @Bean
+    public ServletWebServerFactory serverFactory(){
+        TomcatServletWebServerFactory tomcat = new TomcatServletWebServerFactory();
+        tomcat.addAdditionalTomcatConnectors(createStandardConnector());
+        return tomcat;
+    }
+
+    private Connector createStandardConnector() {
+        Connector connector = new Connector("org.apache.coyote.http11.Http11NioProtocol");
+        connector.setPort(8080);
+        return connector;
+    }*/
+
 
 }
