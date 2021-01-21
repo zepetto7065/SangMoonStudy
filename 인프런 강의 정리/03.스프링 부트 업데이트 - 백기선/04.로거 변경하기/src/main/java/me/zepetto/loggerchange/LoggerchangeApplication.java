32e82e6e37bc00220258fee904ebcae2a8783d91
smyoo <zepetto.yoo@gmail.com>
2021-01-21 19:05:16 +0900

    스프링 부트 업데이터
    02. 의존성 변경
    03. Spring-JCL
    04.

---
+++ 인프런 강의 정리/03.스프링 부트 업데이트 - 백기선/04.로거 변경하기/src/main/java/me/zepetto/loggerchange/LoggerchangeApplication.java
@@ -0,0 +1,13 @@
+package me.zepetto.loggerchange;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LoggerchangeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LoggerchangeApplication.class, args);
+    }
+
+}
