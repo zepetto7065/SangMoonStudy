55a325bc3d7ae05fbfccc5f183df400d117ad1be
smyoo <zepetto.yoo@gmail.com>
2021-03-15 21:28:38 +0900

    스프링 데이터 JPA 환경 구축

---
+++ 인프런 강의 정리/07.실전!스프링데이터JPA/01.프로젝트 환경설정/src/test/java/study/datajpa/repository/MemberJpaRepositoryTest.java
@@ -0,0 +1,37 @@
+package study.datajpa.repository;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.annotation.Rollback;
+import org.springframework.transaction.annotation.Transactional;
+import study.datajpa.entity.Member;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@SpringBootTest
+@Transactional
+@Rollback(false)
+class MemberJpaRepositoryTest {
+
+    @Autowired
+    MemberJpaRepository memberJpaRepository;
+
+
+    @Test
+    public void testMember(){
+        Member member = new Member("memberA");
+        Member savedMember = memberJpaRepository.save(member);
+
+        Member findMember = memberJpaRepository.find(savedMember.getId());
+
+        assertThat(findMember.getId()).isEqualTo(member.getId());
+        assertThat(findMember.getUsername()).isEqualTo(member.getUsername());
+        assertThat(findMember).isEqualTo(member); //JPA가 제공하는 영속성 보장, 같은 트랜잭션에 있기 때문에
+
+
+    }
+
+
+
+}
