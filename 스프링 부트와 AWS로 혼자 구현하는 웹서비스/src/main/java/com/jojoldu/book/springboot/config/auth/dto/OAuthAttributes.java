1abc3353f2185a59ba02d0e9d443042de17f4b86
smyoo <zepetto.yoo@gmail.com>
2021-03-29 22:39:17 +0900

    스프링 부트와 AWS로 혼자 구현하는 웹 서비스
    Spring Security 적용

--- 스프링 부트와 AWS로 혼자 구현하는 웹서비스/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹서비스/src/main/java/com/jojoldu/book/springboot/config/auth/dto/OAuthAttributes.java
@@ -27,7 +27,22 @@ public class OAuthAttributes {
 
     //OAuth2User 에서 반환하는 사용자 정보는 Map이므로 값을 하나하나 반환
     public static OAuthAttributes of(String registrationId, String userNameAttributeName, Map<String, Object> attributes){
+        if("naver".equals(registrationId)){
+            return ofNaver("id",attributes);
+        }
         return ofGoogle(userNameAttributeName, attributes);
+    }
+
+    private static OAuthAttributes ofNaver(String userNameAttributeName, Map<String, Object> attributes) {
+        Map<String,Object> response = (Map<String, Object>) attributes.get("response");
+
+        return OAuthAttributes.builder()
+                .name((String) response.get("name"))
+                .email((String)response.get("email"))
+                .picture((String) response.get("profile_image"))
+                .attributes(response)
+                .nameAttributeKey(userNameAttributeName)
+                .build();
     }
 
     public static OAuthAttributes ofGoogle(String userNameAttributeName, Map<String, Object> attributes){
--- 스프링 부트와 AWS로 혼자 구현하는 웹서비스/src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
+++ 스프링 부트와 AWS로 혼자 구현하는 웹서비스/src/test/java/com/jojoldu/book/springboot/web/IndexControllerTest.java
@@ -5,6 +5,7 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.web.client.TestRestTemplate;
+import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.context.junit4.SpringRunner;
 
 import static org.assertj.core.api.Assertions.assertThat;
@@ -23,7 +24,7 @@ public class IndexControllerTest {
         String body = this.restTemplate.getForObject("/", String.class);
 
         //then
-        assertThat(body).contains("스프링 부트로 시작하는 웹 서비스");
+        assertThat(body).contains("스프링부트로 시작하는 웹 서비스");
     }
 
 
