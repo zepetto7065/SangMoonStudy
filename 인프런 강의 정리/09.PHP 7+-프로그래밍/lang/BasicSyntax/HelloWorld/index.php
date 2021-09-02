50cc47a103e5111601fba69d39ef08d7d0999727
Tony <smyoo@tmon.co.kr>
2021-09-02 19:15:31 +0900

    기본 1강 PHP

---
+++ 인프런 강의 정리/09.PHP 7+-프로그래밍/lang/BasicSyntax/HelloWorld/index.php
@@ -0,0 +1,17 @@
+<?php
+
+// Single Line
+/**
+ * Comments
+ */
+//echo , print 
+//function 이외에도 언어구조
+echo 'Hello, World';
+
+/**
+ * Terminate script
+ */
+//exit;
+die;
+
+print 'Hello, World';
---
+++ 인프런 강의 정리/09.PHP 7+-프로그래밍/lang/BasicSyntax/HtmlTemplate/index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+        <!-- php와 HTML을 함께 사용하는 방법  -->
+        <h1><?php echo 'Hello, world'; ?></h1>
+    </body>
+</html>
