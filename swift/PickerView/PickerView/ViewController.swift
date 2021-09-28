3462da483de094fd48b7a8ea1667c06b59b39443
smyoo <zepetto.yoo@gmail.com>
2021-09-28 17:14:20 +0900

    05강 피커뷰

---
+++ swift/PickerView/PickerView/ViewController.swift
@@ -0,0 +1,67 @@
+//
+//  ViewController.swift
+//  PickerView
+//
+//  Created by smyoo on 2021/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    let MAX_ARRAY_NUM  = 10
+    let PICKER_VIEW_COLUMN = 1
+    let PICKER_VIEW_HEIGHT:CGFloat = 80
+    var imageArray = [UIImage?]()
+    var imageFileName = ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg"]
+
+    
+    @IBOutlet var pickerImage: UIPickerView!
+    @IBOutlet var lblImageFileName: UILabel!
+    @IBOutlet var imageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        //image array에 담는다
+        for i in 0 ..< MAX_ARRAY_NUM {
+            let image = UIImage(named: imageFileName[i])
+            imageArray.append(image)
+        }
+        
+        lblImageFileName.text = imageFileName[0]
+        imageView.image = imageArray[0]
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return PICKER_VIEW_COLUMN
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component:Int) -> Int {
+        return imageFileName.count
+    }
+    
+    //피커뷰 룰렛에 파일명 출력
+//    func pickerView(_ pickerView: UIPickerView, titleForRow row:Int, forComponent component:Int) -> String? {
+//        return imageFileName[row]
+//    }
+    
+    //피커뷰 룰렛에 파일명 대신 이미지 출력
+    func pickerView(_ pickerView: UIPickerView, didSelectRow row:Int, inComponent component:Int){
+        lblImageFileName.text = imageFileName[row]
+        imageView.image = imageArray[row]
+    }
+
+    func pickerView(_ pickerView: UIPickerView, viewForRow row:Int, forComponent component:Int, reusing view: UIView?) -> UIView{
+        let imageView = UIImageView(image:imageArray[row])
+        imageView.frame = CGRect(x:0, y:0, width: 100, height: 150)
+        return imageView
+    }
+
+    //피커 뷰 룰렛 이미지 사이즈 수정
+    func pickerView(_ pickerView: UIPickerView, rowHeightForComponent component: Int) -> CGFloat{
+        return PICKER_VIEW_HEIGHT
+    }
+}
+
