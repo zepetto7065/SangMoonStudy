d7ba542de1dbc88eab84eb98cbd66fc374fe363d
smyoo <zepetto.yoo@gmail.com>
2021-09-29 02:13:16 +0900

    09강 페이지컨트롤

---
+++ swift/PageControl/PageControl/ViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ViewController.swift
+//  PageControl
+//
+//  Created by smyoo on 2021/09/29.
+//
+
+import UIKit
+
+var images = ["01.png","02.png","03.png","04.png","05.png","06.png"]
+
+class ViewController: UIViewController {
+    @IBOutlet var imgView: UIImageView!
+    @IBOutlet var pageControl: UIPageControl!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        
+        pageControl.numberOfPages = images.count
+        
+        pageControl.currentPage = 0
+        
+        pageControl.pageIndicatorTintColor = UIColor.green
+        
+        pageControl.currentPageIndicatorTintColor = UIColor.red
+        
+        imgView.image = UIImage(named:images[0])
+        
+    }
+
+    @IBAction func pageChange(_ sender: UIPageControl) {
+        imgView.image = UIImage(named:images[pageControl.currentPage])
+    }
+    
+}
+
