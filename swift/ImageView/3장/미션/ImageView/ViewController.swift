c2987fba3622b07e3bd9fd6034bd0b5513f85240
smyoo <zepetto.yoo@gmail.com>
2021-09-28 15:47:40 +0900

    3강 imageView

---
+++ swift/ImageView/3장/미션/ImageView/ViewController.swift
@@ -0,0 +1,41 @@
+//
+//  ViewController.swift
+//  ImageView
+//
+//  Created by Ho-Jeong Song on 2020/10/30.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    var maxImage = 6
+    var numImage = 1
+
+    @IBOutlet var imgGallery: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnPrevImage(_ sender: UIButton) {
+        numImage = numImage - 1
+        if (numImage < 1) {
+            numImage = maxImage
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgGallery.image = UIImage(named:imageName)
+    }
+    
+    @IBAction func btnNextImage(_ sender: UIButton) {
+        numImage = numImage + 1
+        if (numImage > maxImage) {
+            numImage = 1
+        }
+        
+        let imageName = String(numImage) + ".png"
+        imgGallery.image = UIImage(named:imageName)
+    }
+}
+
