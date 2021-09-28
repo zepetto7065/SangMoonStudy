dad8578d371c7d520e444263f3c66fbfc2b54185
smyoo <zepetto.yoo@gmail.com>
2021-09-29 02:41:06 +0900

    10강 탭바

---
+++ swift/Tab/Tab/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Tab
+//
+//  Created by smyoo on 2021/09/29.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnMoveImageView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 1
+    }
+    
+    @IBAction func btnMoveDatePickerView(_ sender: UIButton) {
+        tabBarController?.selectedIndex = 2
+    }
+}
+
