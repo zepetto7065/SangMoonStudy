989a84db6dd290c0da6d292b7818642e2f82a6a8
smyoo <zepetto.yoo@gmail.com>
2021-09-28 14:40:05 +0900

    .

---
+++ swift/HelloWorld/HelloWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  HelloWorld
+//
+//  Created by smyoo on 2021/09/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    @IBOutlet var lblHello: UILabel!
+    @IBOutlet var txtName: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnSend(_ sender: UIButton) {
+        lblHello.text = "Hello, " + txtName.text!
+    }
+    
+}
+
