f8502f870222fa1a4878adfd64949a32557f57d6
smyoo <zepetto.yoo@gmail.com>
2021-10-04 14:39:03 +0900

    로또 생성기 앱

---
+++ swift/LottoGenerator/LottoGenerator/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  LottoGenerator
+//
+//  Created by zepetto on 2021/10/04.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+
+    @IBOutlet weak var lblNumberArea: UILabel!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func btnRun(_ sender: UIButton) {
+        var numbers:[Int] = [] //담아둘 배열
+        var result = ""
+
+        while numbers.count < 6 {
+            var number = Int.random(in: 1...45)
+            if !numbers.contains(number){
+                numbers.append(number)
+                result.append(String(number) + " ")
+            }
+        }
+        
+        lblNumberArea.text = result
+    }
+}
+
