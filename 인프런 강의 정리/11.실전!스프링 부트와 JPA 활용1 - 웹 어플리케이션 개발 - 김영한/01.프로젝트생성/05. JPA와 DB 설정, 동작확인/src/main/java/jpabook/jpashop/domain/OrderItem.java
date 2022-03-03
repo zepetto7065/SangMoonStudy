a1e5b1e56db4906ba1e329962a001aad38370752
유상문 <zepetto.yoo@gmail.com>
2022-03-03 21:02:41 +0900

    주문 도메인 개발
    : 주문 도메인 내 비지니스 로직 추가
    : 주문 repository 추가

--- 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/domain/OrderItem.java
+++ 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/domain/OrderItem.java
@@ -25,5 +25,28 @@ public class OrderItem {
     private int orderPrice;
     private int count;
 
+    //==생성 메서드==//
+    //나중에 할인 시스템(쿠폰이 들어갈수 있음)
+    public static OrderItem createOrderItem(Item item, int orderPrice, int count){
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
 
+        item.removeStock(count);
+        return orderItem;
+    }
+
+    //==비지니스 로직==//
+    public void cancel() {
+        getItem().addStock(count); //재고 수량 원복
+    }
+
+    /**
+     * 주문상품 전체 가격 조회
+     */
+    //==조회 로직==//
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
