cd4b8413526e8ce05ef44806f0a8ce8e4ba495f0
유상문 <zepetto.yoo@gmail.com>
2022-03-12 21:09:27 +0900

    변경감지&Merge

--- 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/service/ItemService.java
+++ 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/service/ItemService.java
@@ -1,5 +1,6 @@
 package jpabook.jpashop.service;
 
+import jpabook.jpashop.domain.item.Book;
 import jpabook.jpashop.domain.item.Item;
 import jpabook.jpashop.repository.ItemRepository;
 import lombok.RequiredArgsConstructor;
@@ -17,6 +18,16 @@ public class ItemService {
     @Transactional
     public void saveItem(Item item){
         itemRepository.save(item);
+    }
+
+    @Transactional
+    public void updateItem(Long itemId, Book param){
+        Item findItem = itemRepository.findOne(itemId);//영속성 객체 찾아옴
+        findItem.setPrice(param.getPrice());
+        //save도 뭐고 아무것도 호출할 필요없다. 이미 영속상태
+        //트랜잭션 커밋에 의해 JPA는 flush를 날린다.
+        //변경된 애들? 찾아 찾아 DB에 update쳐버린다.
+//        findItem.change(price, name, stockQuantity);
     }
 
     public List<Item> findItem(){
