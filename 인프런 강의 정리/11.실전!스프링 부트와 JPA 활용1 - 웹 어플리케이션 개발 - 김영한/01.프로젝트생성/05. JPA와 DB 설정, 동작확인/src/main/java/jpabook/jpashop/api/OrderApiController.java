97ae036af7e0aaff9b5385b016b2ef12b520aad0
유상문 <zepetto.yoo@gmail.com>
2022-04-13 16:59:56 +0900

    성능최적화 V1-V6

---
+++ 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/api/OrderApiController.java
@@ -0,0 +1,125 @@
+package jpabook.jpashop.api;
+
+import jpabook.jpashop.domain.Address;
+import jpabook.jpashop.domain.Order;
+import jpabook.jpashop.domain.OrderItem;
+import jpabook.jpashop.domain.OrderStatus;
+import jpabook.jpashop.repository.OrderRepository;
+import jpabook.jpashop.repository.OrderSearch;
+import jpabook.jpashop.repository.order.query.OrderQueryDto;
+import jpabook.jpashop.repository.order.query.OrderQueryRepository;
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDateTime;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@RequiredArgsConstructor
+public class OrderApiController {
+
+    private final OrderRepository orderRepository;
+    private final OrderQueryRepository orderQueryRepository;
+
+    @GetMapping("/api/v1/orders")
+    public List<Order> orderV1(){
+        List<Order> all = orderRepository.findAllByString(new OrderSearch());
+        for (Order order : all) {
+            order.getMember().getName();
+            order.getDelivery().getAddress();
+
+            List<OrderItem> orderItems = order.getOrderItems();
+            orderItems.stream().forEach(o->o.getItem().getName());
+        }
+        return all;
+    }
+
+    @GetMapping("/api/v2/orders")
+    public List<OrderDto> orderV2(){
+        List<Order> orders = orderRepository.findAllByString(new OrderSearch());
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3/orders")
+    public List<OrderDto> orderV3(){
+        List<Order> orders = orderRepository.findAllWithItem();
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v3.1/orders")
+    public List<OrderDto> orderV3_page(@RequestParam(value = "offset",defaultValue = "0") int offset,
+            @RequestParam(value = "limit", defaultValue = "100") int limit )
+    {
+        List<Order> orders = orderRepository.findAllWithMemberDelivery(offset, limit);
+        //orderItemss는?
+
+        return orders.stream()
+                .map(OrderDto::new)
+                .collect(Collectors.toList());
+    }
+
+    @GetMapping("/api/v4/orders")
+    public List<OrderQueryDto> orderV4()
+    {
+        return orderQueryRepository.findOrderQueryDtos();
+    }
+
+    @GetMapping("/api/v5/orders")
+    public List<OrderQueryDto> orderV5()
+    {
+        return orderQueryRepository.findAllByDto_optimization();
+    }
+
+//    @GetMapping("/api/v6/orders")
+//    public List<OrderQueryDto> orderV6()
+//    {
+//        List<OrderFlatDto> flats = orderQueryRepository.findAllByDto_flat();
+//        //중복을 거르는 작업을 하자
+//        return ...
+//    }
+
+
+    @Getter
+    static class OrderDto {
+        private Long orderId;
+        private String name;
+        private LocalDateTime orderDate;
+        private OrderStatus orderStatus;
+        private Address address;
+        private List<OrderItemDto> orderItems;
+
+        public OrderDto(Order o) {
+            orderId = o.getId();
+            name = o.getMember().getName();
+            orderDate = o.getOrderDate();
+            orderStatus = o.getStatus();
+            address = o.getMember().getAddress();
+            o.getOrderItems().forEach(order -> order.getItem().getName());
+            orderItems = o.getOrderItems().stream()
+                    .map(OrderItemDto::new)
+                    .collect(Collectors.toList());
+        }
+    }
+
+    @Getter
+    static class  OrderItemDto{
+
+        private String itemName;
+        private int orderPrice;
+        private int count;
+
+        public OrderItemDto(OrderItem orderItem) {
+            itemName = orderItem.getItem().getName();
+            orderPrice = orderItem.getOrderPrice();
+            count = orderItem.getOrderPrice();
+        }
+    }
+}
--- 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/domain/Order.java
+++ 인프런 강의 정리/11.실전!스프링 부트와 JPA 활용1 - 웹 어플리케이션 개발 - 김영한/01.프로젝트생성/05. JPA와 DB 설정, 동작확인/src/main/java/jpabook/jpashop/domain/Order.java
@@ -5,6 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 import org.aspectj.weaver.ast.Or;
+import org.hibernate.annotations.BatchSize;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
