a9dfea3b13c1642b73929d9da2a338b545e15756
유상문 <zepetto.yoo@gmail.com>
2022-08-16 09:06:19 +0900

    인스타그램 클론 기능구현 완료

---
+++ flutter/instagram-clon/lib/account_page.dart
@@ -0,0 +1,113 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+
+class AccountPage extends StatefulWidget {
+  final User user;
+  AccountPage(this.user);
+
+  @override
+  State<AccountPage> createState() => _AccountPageState();
+}
+
+class _AccountPageState extends State<AccountPage> {
+  GoogleSignIn _googleSignIn = GoogleSignIn();
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: _buildAppbar(),
+      body: _buildBody(),
+    );
+  }
+
+  Widget _buildAppbar() {
+    return AppBar(
+      backgroundColor: Colors.white,
+      actions: <Widget>[
+        IconButton(
+            onPressed: () {
+              FirebaseAuth.instance.signOut();
+              _googleSignIn.signOut();
+            },
+            icon: Icon(Icons.exit_to_app),
+            color: Colors.black),
+      ],
+    );
+  }
+
+  Widget _buildBody() {
+    return Padding(
+      padding: EdgeInsets.all(16.0),
+      child: Row(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
+        children: <Widget>[
+          Column(
+            children: <Widget>[
+              Stack(
+                children: <Widget>[
+                  SizedBox(
+                    width: 80.0,
+                    height: 80.0,
+                    child: CircleAvatar(
+                      backgroundImage: NetworkImage(
+                          widget.user.photoURL),
+                    ),
+                  ),
+                  Container(
+                    width: 80.0,
+                    height: 80.0,
+                    alignment: Alignment.bottomRight,
+                    child: Stack(
+                      alignment: Alignment.center,
+                      children: <Widget>[
+                        SizedBox(
+                          width: 28.0,
+                          height: 28.0,
+                          child: FloatingActionButton(onPressed: null,
+                            backgroundColor: Colors.white,
+                          ),
+                        ),
+                        SizedBox(
+                          width: 25.0,
+                          height: 25.0,
+                          child: FloatingActionButton(onPressed: null,
+                            backgroundColor: Colors.blue,
+                            child: Icon(Icons.add),
+                          ),
+                        )
+                      ],
+                    ),
+                  )
+                ],
+              ),
+              Padding(
+                padding: EdgeInsets.all(4.0),
+              ),
+              Text(
+                widget.user.displayName,
+                style: TextStyle(fontWeight: FontWeight.bold, fontSize: 18.0),
+              )
+            ],
+          ),
+          Text(
+            '0\n게시물',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            '0\n팔로워',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+          Text(
+            '0\n팔로잉',
+            textAlign: TextAlign.center,
+            style: TextStyle(fontSize: 18.0),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- flutter/instagram-clon/lib/login_page.dart
+++ flutter/instagram-clon/lib/login_page.dart
@@ -1,12 +1,53 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_signin_button/flutter_signin_button.dart';
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:instagram_clon/tab_page.dart';
 
 class LoginPage extends StatelessWidget {
-  const LoginPage({Key? key}) : super(key: key);
+  final GoogleSignIn _googleSignIn = GoogleSignIn();
+  final FirebaseAuth _auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Text('LoginPage'),
+    return Scaffold(
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Instagram Clon',
+              style: TextStyle(fontSize: 40.0, fontWeight: FontWeight.bold),
+            ),
+            Padding(
+              padding: EdgeInsets.all(50.0),
+            ),
+            SignInButton(
+              Buttons.Google,
+              onPressed: () {
+                _handleSignIn().then((user){
+                  Navigator.pushReplacement(context,
+                      MaterialPageRoute(builder: (context)=>TabPage(user.user))
+                  );
+                });
+              },
+            ),
+          ],
+        ),
+      ),
     );
   }
+
+  Future<UserCredential> _handleSignIn() async {
+    GoogleSignInAccount googleUser = await _googleSignIn.signIn();
+    GoogleSignInAuthentication googleAuth = await googleUser.authentication;
+
+    final GoogleAuthCredential credential = GoogleAuthProvider.credential(
+      accessToken: googleAuth.accessToken,
+      idToken: googleAuth.idToken,
+    );
+
+    return await FirebaseAuth.instance.signInWithCredential(credential);
+  }
 }
+
